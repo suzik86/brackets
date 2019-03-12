620703c2de7cc692b63fa89eef0ae109b055733f
Irina Shishkova <i.suzanovich@mail.ru>
2019-03-12 17:12:02 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  
+    let stack = [];    
+    
+    let couples = bracketsConfig.reduce(function(prev, curr) {
+      prev[curr[1]] = curr[0];
+      return prev;
+    }, {});
+
+    let opens = Object.values(couples);
+
+    for (var i = 0; i < str.length; i++) {
+      let cur = str[i];
+      let isClose = couples.hasOwnProperty(cur);
+      let isOpen = opens.includes(cur);
+
+      if (isOpen || isClose){
+        stack.push(cur);        
+      } 
+      if (stack.length > 1 && isClose && couples[cur] == stack[stack.length - 2]) {
+          stack.pop();
+          stack.pop();
+      } 
+    }
+
+    return stack.length == 0;
+  }
